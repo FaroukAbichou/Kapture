d448017d890b884fb1bdca3a3151bc2df0bd8da9
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-02 09:54:51 +0100

    Some Changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,8 @@ repositories {
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation("org.bytedeco:javacv-platform:1.5.9")
 }
 
 compose.desktop {
