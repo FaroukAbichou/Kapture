4f3805ace285ce12ea329fd559de058604898192
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-03 15:04:28 +0100

    -Some Changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,15 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.application
+import org.bytedeco.javacv.CanvasFrame
+import org.bytedeco.javacv.FFmpegFrameGrabber
 
 @Composable
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
         Button(onClick = {
             text = "Hello, Desktop!"
@@ -24,8 +20,22 @@ fun App() {
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+fun main()  {
+    val x = 0
+    val y = 0
+    val w = 1024
+    val h = 768 // specify the region of screen to grab
+    val grabber = FFmpegFrameGrabber("1")
+    grabber.format = "yuyv422"
+    grabber.imageWidth = w
+    grabber.imageHeight = h
+    grabber.start()
+
+    val frame = CanvasFrame("Screen Capture")
+    while (frame.isVisible) {
+        frame.showImage(grabber.grab())
     }
+    frame.dispose()
+    grabber.stop()
+    println("Done.")
 }
