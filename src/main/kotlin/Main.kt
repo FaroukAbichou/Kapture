0d4cc9a325b4eda68f701ab27e14c307662ff38f
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-03 17:44:44 +0100

    - Some Changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,39 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun App(screenRecorder: ScreenRecorder) {
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Button(onClick = {
+                screenRecorder.record()
+            }) {
+                Text("Record")
+            }
         }
     }
 }
 
-fun main()  {
-    val outputFile = "output.mp4"
-    ScreenRecorder(outputFile).record()
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Screen Recorder"
+    ) {
+        val screenRecorder = ScreenRecorder("output.mp4")
+        App(screenRecorder)
+    }
 }
