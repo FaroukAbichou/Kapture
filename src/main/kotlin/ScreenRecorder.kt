367944444381cd477e9ae3f300a9b8c47324d153
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-04 22:27:46 +0100

    - Some Changes

--- src/main/kotlin/ScreenRecorder.kt
+++ src/main/kotlin/ScreenRecorder.kt
@@ -13,13 +13,13 @@ class ScreenRecorder(private val config: ConfigurationManager) {
         val ffprobe = FFprobe("/opt/homebrew/bin/ffprobe")
 
         val builder = FFmpegBuilder()
-            .setInput("1") // '1' typically represents the main screen in AVFoundation on macOS
-            .setFormat("avfoundation") // Use the AVFoundation format
-            .addOutput(outputFilePath) // Specify the output file path
-            .setDuration(durationInSeconds.toLong(), TimeUnit.SECONDS) // Set the recording duration
-            .setVideoCodec(config.videoCodecName) // Use the x264 codec for video
-            .setVideoFrameRate(config.frameRate, 1) // Record at 30 frames per second
-            .setVideoResolution(config.width, config.height) // Set the desired resolution
+            .setInput("1")
+            .setFormat("avfoundation")
+            .addOutput(outputFilePath)
+            .setDuration(durationInSeconds.toLong(), TimeUnit.SECONDS)
+            .setVideoCodec(config.videoCodecName)
+            .setVideoFrameRate(config.frameRate, 1)
+            .setVideoResolution(config.width, config.height)
             .done()
 
         val executor = FFmpegExecutor(ffmpeg, ffprobe)
