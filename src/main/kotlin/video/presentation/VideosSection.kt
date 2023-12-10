8b775f0f1b429bc2c5484cd9c2aac29636e31a8b
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-10 14:23:49 +0100

    Some Changes

--- src/main/kotlin/video/presentation/VideosSection.kt
+++ src/main/kotlin/video/presentation/VideosSection.kt
@@ -1,16 +1,16 @@
 package video.presentation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import core.util.FilePaths
+import androidx.compose.runtime.saveable.rememberSaveable
 import video.presentation.event.VideoEvent
 import video.presentation.state.Video
+import core.util.FilePaths
 
 @Composable
 fun VideosSection() {
