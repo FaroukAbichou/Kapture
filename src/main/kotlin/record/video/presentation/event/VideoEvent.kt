1830322811c937348701d2f72b86ded752f33c92
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-15 16:00:55 +0100

    - not working for distribution

--- src/main/kotlin/record/video/presentation/event/VideoEvent.kt
+++ src/main/kotlin/record/video/presentation/event/VideoEvent.kt
@@ -4,34 +4,15 @@ import probe.domain.WindowPlacement
 import record.video.domain.model.RecordSettings
 
 sealed class VideoEvent {
-    data class GetVideosByPath(val path: String) : VideoEvent()
-
-    data class RecordSection(
-        val config: RecordSettings,
-        val windowPlacement: WindowPlacement?,
-    ) : VideoEvent()
-
-    data class Record(
-        val config: RecordSettings,
-        val windowPlacement: WindowPlacement?,
-    ) : VideoEvent()
-
-    data class StartRecording(
-        val config: RecordSettings,
-        val bounds: WindowPlacement,
-    ) : VideoEvent()
-
-    data class SelectScreen(
-        val screenId: String,
-    ) : VideoEvent()
-
+    data object RecordDevice: VideoEvent()
     data object StopRecording : VideoEvent()
     data object SelectScreenSection : VideoEvent()
-
+    data class SelectVideo(val path: String) : VideoEvent()
+    data class DeleteVideo(val path: String) : VideoEvent()
+    data class GetVideosByPath(val path: String) : VideoEvent()
+    data class SelectScreen(val screenId: String) : VideoEvent()
     data class RecordAllWindows(val config: RecordSettings) : VideoEvent()
-    class RecordAudio(val config: RecordSettings) : VideoEvent()
-    data object RecordDevice: VideoEvent()
-
-    data class SelectVideosLocation(val path: String) : VideoEvent()
-
+    data class StartRecording(val config: RecordSettings,val bounds: WindowPlacement) : VideoEvent()
+    data class Record(val config: RecordSettings, val windowPlacement: WindowPlacement?) : VideoEvent()
+    data class RecordSection(val config: RecordSettings, val windowPlacement: WindowPlacement?) : VideoEvent()
 }
