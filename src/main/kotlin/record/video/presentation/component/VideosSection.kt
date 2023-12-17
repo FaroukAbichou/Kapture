71e7047c7485be7fcfaaace5169270c8f664967e
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-17 11:13:57 +0100

    Some Changes

--- src/main/kotlin/record/video/presentation/component/VideosSection.kt
+++ src/main/kotlin/record/video/presentation/component/VideosSection.kt
@@ -1,6 +1,7 @@
 package record.video.presentation.component
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
@@ -9,9 +10,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import core.components.KpFilterBar
 import core.util.FilePaths
 import record.home.presentation.component.KpSearchBar
-import record.video.domain.model.Video
 import record.video.presentation.event.VideoEvent
 import record.video.presentation.state.VideoState
 
@@ -32,13 +33,24 @@ fun VideosSection(
         mutableStateOf("")
     }
 
-    Column(
-        horizontalAlignment = Alignment.Start,
+    Row (
+        horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
     ) {
         KpSearchBar(
             modifier = Modifier.height(200.dp),
             searchQuery = searchQuery,
+            searchResults = searchedVideos,
+            onSearchQueryChange = { query ->
+                searchQuery = query
+                searchedVideos = state.videos.filter { video ->
+                    video.name.contains(query, ignoreCase = true)
+                }
+            }
+        )
+        KpFilterBar(
+            modifier = Modifier.height(200.dp),
+            filterQuery = searchQuery,
             searchResults = searchedVideos,
             onSearchQueryChange = { query ->
                 searchQuery = query
@@ -65,4 +77,3 @@ fun VideosSection(
         }
     }
 }
-
