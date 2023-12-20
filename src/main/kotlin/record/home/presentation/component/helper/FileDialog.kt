43e4a0f4e9afe42f77ccca3159f0fffc216a5023
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-20 17:03:09 +0100

    - Working on SelectOutputLocationSection

--- src/main/kotlin/record/home/presentation/component/helper/FileDialog.kt
+++ src/main/kotlin/record/home/presentation/component/helper/FileDialog.kt
@@ -1,8 +1,5 @@
 
 import androidx.compose.runtime.*
-import androidx.compose.ui.window.DialogWindow
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.awt.FileDialog
 import java.awt.Frame
 import java.io.FilenameFilter
@@ -11,40 +8,64 @@ import javax.swing.SwingUtilities
 @Composable
 fun FileDialog(
     title: String,
-    isOpen: Boolean,
+    isOpen: MutableState<Boolean>,
     fileExtensions: Set<String>,
     onResult: (String?) -> Unit
 ) {
     var dialog by remember { mutableStateOf<FileDialog?>(null) }
 
-    LaunchedEffect(isOpen) {
-        println( "FileDialog: $isOpen")
-        if (isOpen) {
-            withContext(Dispatchers.IO) {
-                dialog = FileDialog(null as Frame?, title).apply {
-                    isMultipleMode = false
-                    mode = FileDialog.LOAD
-                    filenameFilter = FilenameFilter { _, name ->
-                        fileExtensions.any { name.endsWith(it) }
-                    }
+    if (isOpen.value && dialog == null) {
+        SwingUtilities.invokeLater {
+            dialog = FileDialog(null as Frame?, title).apply {
+                isMultipleMode = false
+                mode = FileDialog.LOAD
+                filenameFilter = FilenameFilter { _, name ->
+                    fileExtensions.any { name.endsWith(it) }
                 }
-
+                isVisible = true
+                val file = dialog?.file
                 SwingUtilities.invokeLater {
-                    val files = dialog?.files?.map { it.path }
-                    onResult(files?.firstOrNull())
-                    dialog = null
+                    onResult(file)
                 }
+                isOpen.value = false
+                dialog = null
+            }
+
+        }
+//        dialog?.addWindowListener(object : WindowAdapter() {
+//            override fun windowClosing(e: WindowEvent?) {
+//                // When the dialog is closing, retrieve the selected file and invoke onResult.
+//                println( "FileDialog file: ${dialog?.file}")
+//                val file = dialog?.file // Retrieve the selected file path.
+//                SwingUtilities.invokeLater {
+//                    onResult(file) // Provide the result to the callback.
+//                }
+//                isOpen.value = false // Reset the open state.
+//                dialog = null // Dispose of the dialog.
+//            }
+//        })
+
+    }
+
+    if (!isOpen.value) {
+        dialog?.let {
+            SwingUtilities.invokeLater {
+                it.dispose()
+                dialog = null // Reset the dialog reference.
             }
         }
     }
 
-    dialog?.let {
-        DialogWindow(
-            onCloseRequest = { onResult(null) },
-            title = title,
-            content = {
-
+    // This DisposableEffect ensures that the dialog is properly disposed of
+    // when this Composable is removed from the composition.
+    DisposableEffect(Unit) {
+        onDispose {
+            dialog?.let {
+                SwingUtilities.invokeLater {
+                    it.dispose() // Dispose of the dialog on the EDT.
+                    dialog = null // Reset the dialog reference.
+                }
             }
-        )
+        }
     }
 }
