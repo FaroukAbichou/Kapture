51d517157485b5278b6c18f956b3b1594eceb9e7
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-24 20:55:29 +0100

    - Working on SettingsScreen

--- src/main/kotlin/record/settings/presentation/SettingsScreen.kt
+++ src/main/kotlin/record/settings/presentation/SettingsScreen.kt
@@ -1,9 +1,13 @@
 package record.settings.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import core.components.SelectOutputLocationSection
@@ -29,5 +33,50 @@ fun SettingsScreen(
             modifier = Modifier
                 .padding(16.dp)
         )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+        ) {
+
+            Column(
+                modifier = Modifier
+            ){
+                state.screens.forEach {
+                    Text(
+                        text = it.name,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        modifier = Modifier,
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier
+            ){
+                state.cameras.forEach {
+                    Text(
+                        text = it.name,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        modifier = Modifier,
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier
+            ){
+                state.audioSources.forEach {
+                    Text(
+                        text = it.name,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        modifier = Modifier,
+                    )
+                }
+            }
+        }
     }
 }
