db234419976924237ecf09725d2c0a9381aea90a
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2024-01-07 21:26:02 +0100

    - changing to bytedeco wrapper

--- src/main/kotlin/record/video/data/VideoRepositoryImpl.kt
+++ src/main/kotlin/record/video/data/VideoRepositoryImpl.kt
@@ -5,75 +5,31 @@ import core.util.FileHelper.VideoExtensions
 import core.util.FileHelper.getFileDate
 import core.util.FileHelper.getFileSize
 import core.util.FileHelper.getFilesWithExtension
-import org.monte.media.av.Format
-import org.monte.media.av.FormatKeys
-import org.monte.media.av.codec.video.VideoFormatKeys
-import org.monte.media.math.Rational
-import org.monte.media.screenrecorder.ScreenRecorder
 import probe.core.WindowPlacement
 import probe.screen.domain.model.Screen
 import record.video.domain.VideoRepository
 import record.video.domain.model.RecordSettings
 import record.video.domain.model.Video
-import java.awt.GraphicsEnvironment
-import java.io.File
-import java.io.IOException
 import java.nio.file.Path
 import kotlin.time.Duration
 
 class VideoRepositoryImpl : VideoRepository {
-    private var screencorder: Screencorder? = null
-    private var screenRecorder: ScreenRecorder? = null
-    private val movieFolder: String = System.getProperty("user.home") + File.separator + "Videos"
 
-    init {
-        // Make sure the directory exists
-        screencorder = Screencorder()
-        screencorder
-    }
+
     override fun startRecording(
         windowPlacement: WindowPlacement?,
         selectedScreen: Screen,
     ) {
-        val gc = GraphicsEnvironment
-            .getLocalGraphicsEnvironment()
-            .defaultScreenDevice
-            .defaultConfiguration
+        val recorder = Screencorder()
 
-        val fileFormat = Format(
-            FormatKeys.MediaTypeKey, FormatKeys.MediaType.FILE,
-            FormatKeys.MimeTypeKey, FormatKeys.MIME_AVI
-        )
-        val screenFormat = Format(
-            FormatKeys.MediaTypeKey, FormatKeys.MediaType.VIDEO,
-            FormatKeys.EncodingKey, VideoFormatKeys.ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
-            VideoFormatKeys.CompressorNameKey, VideoFormatKeys.ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
-            VideoFormatKeys.DepthKey, 24,
-            FormatKeys.FrameRateKey, Rational.valueOf(15.0),
-            VideoFormatKeys.QualityKey, 1.0f,
-            FormatKeys.KeyFrameIntervalKey, (15 * 60)
-        )
-        val mouseFormat = Format(
-            FormatKeys.MediaTypeKey, FormatKeys.MediaType.VIDEO,
-            FormatKeys.EncodingKey, ScreenRecorder.ENCODING_BLACK_CURSOR,
-            FormatKeys.FrameRateKey, Rational.valueOf(30.0)
-        )
-
-
-        screenRecorder?.start()
-
+        recorder.start()
+        Thread.sleep(java.time.Duration.ofSeconds(7).toMillis())
+        println("Stopping")
+        println(recorder.movieFolder)
+        recorder.stop()
     }
     override fun stopRecording() {
-        // Stop recording
-        try {
-            screencorder?.stop()
-        } catch (e: IOException) {
-            e.printStackTrace()
-            // Handle exception, possibly throw a custom exception or log it
-        }
-
-        // Clean up
-        screencorder = null
+//        screencorder.stop()
     }
 
     override fun recordScreenWithTimeout(
